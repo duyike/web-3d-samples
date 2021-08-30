2d9bb7ef69ee444fdb96b35e1aa29d0b128ca348
duyike <sy20030260@gmail.com>
2021-08-31 00:26:08 +0800

    delete unuse file

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -2,7 +2,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-const pages = ["index", "three-js-starting", "flag", "cloth"];
+const pages = ["index", "flag"];
 
 module.exports = {
   entry: pages.reduce((config, page) => {
